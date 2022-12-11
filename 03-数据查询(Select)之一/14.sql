a3bff2c64514e322f91b515e9705bc62656e2279
Miracle_zjy <Miracle_zjy@outlook.com>
2022-12-11 17:07:30 +0800

    upload other solutions

--- 03-数据查询(Select)之一/14.sql
+++ 03-数据查询(Select)之一/14.sql
@@ -1,9 +1,9 @@
 -- 第 14 关：第 N 高问题
-SELECT
-    i_id,
-    i_amount
-FROM
-    (
+
+-- 1. 方案一
+
+SELECT i_id, i_amount
+FROM (
         SELECT
             i_id,
             i_amount,
@@ -11,10 +11,19 @@ FROM
                 ORDER BY
                     i_amount DESC
             ) AS rk
-        FROM
-            insurance
+        FROM insurance
     ) t
-WHERE
-    rk = 4
-ORDER BY
-    i_id;
+WHERE rk = 4
+ORDER BY i_id;
+
+-- 2. 方案二
+
+SELECT i_id, i_amount
+FROM insurance
+WHERE i_amount = (
+        SELECT
+            DISTINCT i_amount
+        FROM insurance
+        ORDER BY i_amount DESC
+        limit 3, 1
+    );
--- 03-数据查询(Select)之一/6.sql
+++ 03-数据查询(Select)之一/6.sql
@@ -1,23 +1,35 @@
 -- 第 6 关：商品收益的众数
+
+-- 1. 方案一
+
 SELECT
     b.pro_income,
     b.cnt AS presence
-FROM
-    (
+FROM (
         SELECT
             a.pro_income,
             a.cnt,
             MAX(a.cnt) over() AS max_cnt
-        FROM
-            (
+        FROM (
                 SELECT
                     pro_income,
                     COUNT(*) AS cnt
-                FROM
-                    property
+                FROM property
                 GROUP BY
                     pro_income
             ) AS a
     ) AS b
-where
-    b.cnt = b.max_cnt;
+where b.cnt = b.max_cnt;
+
+-- 2. 方案二
+
+SELECT
+    pro_income,
+    COUNT(*) AS presence
+FROM property
+GROUP BY pro_income
+HAVING COUNT(*) >= ALL (
+        SELECT COUNT(*)
+        FROM property
+        GROUP BY pro_income
+    );
